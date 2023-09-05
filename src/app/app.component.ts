58bae7b1f54b3160ac86b0442ff95c278face80d
Abner Rodrigues <abner.rds01@gmail.com>
2023-09-05 14:20:39 -0300

    LC - V1 - OnInit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
+import { Component, OnInit } from '@angular/core';
+import { Item } from './interfaces/iItem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app-lista-de-compras';
+  ListaDeCompra! : Array<Item>
 
-  constructor() { }
+   constructor(private listaService: ListaDeCompraService) { }
+
+   ngOnInit(){
+      this.ListaDeCompra = this.listaService.getListaDeCompra();
+      console.log(this.ListaDeCompra)
+   }
 }
